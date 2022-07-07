11f684ddfe8b6a7d384c174495b0af7b865591ff
nitinnavik <naviknitin143@gmail.com>
2022-07-07 21:19:06 +0530

    comments.

--- src/API/weatherApi.js
+++ src/API/weatherApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 var API_KEY = "039785c140a57c78992ac26af76998a5";
 
+//For fetching the Country Data by Search input
 export const fetchCountry = async (data) => {
   console.log(data);
   let url = `http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&appid=${API_KEY}`;
@@ -13,6 +14,7 @@ export const fetchCountry = async (data) => {
   return response.data.list;
 };
 
+//For Fetching the Weather Data by Id
 export const fetchById = async (id) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?id=1269750&appid=${API_KEY}`;
     if(id){
@@ -24,6 +26,7 @@ export const fetchById = async (id) => {
   return response.data;
 };
 
+//Fetching weather Data by Latitude and Longitude
 export const fetchByLatLon = async (res) => {
   console.log(res);
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=20&lon=77&appid=${API_KEY}`;
@@ -36,6 +39,7 @@ export const fetchByLatLon = async (res) => {
   return response.data;
 };
 
+//Fetching Data for initial Render
 export const fetchInitial = async (res) => {
    
     let url = `https://api.openweathermap.org/data/2.5/onecall?lat=20&lon=77&appid=${API_KEY}`;
